b06b6aed7d38758204e2a6eddd577cf8f38517ae
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-11-15 10:03:16 -0300

    creado el objeto enterprise con sus metodos getters y setters, ademas de sus constructores

--- src/main/java/com/emagomiladev/app/entities/Enterprise.java
+++ src/main/java/com/emagomiladev/app/entities/Enterprise.java
@@ -1,5 +1,87 @@
 package com.emagomiladev.app.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "enterprises")
 public class Enterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEnterprise;
+
+    private String name;
+    private String cuit;
+    private String direccion;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    private String email;
+
+    public Enterprise() {
+        super();
+    }
+
+    public Enterprise(Long idEnterprise, String name, String cuit, String direccion, String phoneNumber, String email) {
+        this.idEnterprise = idEnterprise;
+        this.name = name;
+        this.cuit = cuit;
+        this.direccion = direccion;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Long getIdEnterprise() {
+        return idEnterprise;
+    }
+
+    public void setIdEnterprise(Long idEnterprise) {
+        this.idEnterprise = idEnterprise;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(String cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
