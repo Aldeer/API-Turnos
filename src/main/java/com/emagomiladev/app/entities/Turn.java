3f4cb37399465fa4d0726b69f3226cea7d5f3ee3
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-11-16 10:23:22 -0300

    entidad turno creada, con metodos y constructores

---
+++ src/main/java/com/emagomiladev/app/entities/Turn.java
@@ -0,0 +1,14 @@
+package com.emagomiladev.app.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "turns")
+public class Turn implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
